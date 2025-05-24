215f59da76bce7f73f254b50e144ea983899b8d6
Joao Davi Rigo Mazzarolo <jdrmazzarolo@outlook.com>
2025-05-24 19:12:37 -0300

    feat: removing logging files

--- main.py
+++ main.py
@@ -17,29 +17,21 @@ import csv
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mean", type=int, default=5)
-    parser.add_argument("--n_features", type=int, default=0)
-    parser.add_argument("--start_point", type=int, default=0)
     args = parser.parse_args()
 
     mean = args.mean
-    conf.N_FEATURES = args.n_features
-    conf.START_POINT = args.start_point
 
     data = Data(ProcessWineData, mean_rows=mean, perc_val=0.1, perc_test=0.1) 
-    
     model = NN(data=data,lr=1e-6)
     
-    seed_everything(42, workers=True)
-    
+    seed_everything(42, workers=True)   
     trainer = Trainer(max_epochs=10000, 
                       log_every_n_steps=10,
                       callbacks=[
                           EarlyStopping(monitor="val_loss",mode='max',patience=25,stopping_threshold=1)
                           ],
-                      deterministic=True,enable_checkpointing=False)
-    
+                      deterministic=True,enable_checkpointing=False) 
     trainer.fit(model, datamodule=data)
-
     trainer.test(model, datamodule=data)
 
 if __name__=='__main__':
